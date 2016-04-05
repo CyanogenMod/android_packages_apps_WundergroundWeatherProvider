dda60c79896d803d944abfa022c779982f9ad88a
Adnan Begovic <adnan@cyngn.com>
2016-04-05 14:15:15 -0700

    WundergroundCM: Initial checking of WG weather provider service.
    Change-Id: If2f638889578180d3b7bf7c8bebe1caee69fec52

---
+++ app/src/main/java/org/cyanogenmod/wundergroundcmweatherprovider/wunderground/responses/forecast/TemperatureForecastResponse.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.wundergroundcmweatherprovider.wunderground.responses.forecast;
+
+import java.io.Serializable;
+
+public class TemperatureForecastResponse implements Serializable {
+    private int fahrenheit;
+    private int celsius;
+
+    public int getFahrenheit() {
+        return fahrenheit;
+    }
+
+    public void setFahrenheit(int fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    public int getCelsius() {
+        return celsius;
+    }
+
+    public void setCelsius(int celsius) {
+        this.celsius = celsius;
+    }
+}
